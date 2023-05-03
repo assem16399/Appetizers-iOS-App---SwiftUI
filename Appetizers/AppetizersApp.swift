56636799a0aaa79b65b98229eb1399c9bdb5df63
assem16399 <aasem.hany@gmail.com>
2023-05-03 12:55:03 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Aasem Hany on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
